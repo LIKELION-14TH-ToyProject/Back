ba9694fcd61d223b7d6243ac70012e67edd5e0d6
Leeseoin <tnwn0225@gmail.comm>
2026-05-17 16:46:54 +0900

    fix: allow null birth field

--- accounts/models.py
+++ accounts/models.py
@@ -3,5 +3,5 @@ from django.contrib.auth.models import AbstractUser
 
 class User(AbstractUser):
     nickname = models.CharField(max_length=30)
-    birth = models.DateField()
+    birth = models.DateField(null=True, blank=True)
     purpose = models.CharField(max_length=100)
