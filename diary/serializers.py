d1cc8c2ccd12c5192a72d3756f2d9682d6837126
Leeseoin <tnwn0225@gmail.comm>
2026-05-17 17:13:03 +0900

    fix: tag

--- diary/serializers.py
+++ diary/serializers.py
@@ -45,7 +45,7 @@ class PostSerializer(serializers.ModelSerializer):
         return post
     
     def update(self, instance, validated_data):
-        tags = validated_data.pop('tags', [])
+        tags = validated_data.pop('tags', None)
 
         instance.title = validated_data.get('title', instance.title)
         instance.body = validated_data.get('body', instance.body)
@@ -56,12 +56,15 @@ class PostSerializer(serializers.ModelSerializer):
         if tags is not None: # 태그 값이 실제로 들어왔을 떄만 수정
             instance.tags.clear()
 
-        for tag_name in tags:
-            tag, created = Tag.objects.get_or_create(name=tag_name)
-            instance.tags.add(tag)
+            for tag_name in tags:
+                tag_name = tag_name.strip()
+
+                if tag_name:
+                    tag, created = Tag.objects.get_or_create(name=tag_name)
+                    instance.tags.add(tag)
         
-        # 게시글이 하나도 없는 태그 삭제
-        Tag.objects.filter(posts__isnull=True).delete()
+            # 게시글이 하나도 없는 태그 삭제
+            Tag.objects.filter(posts__isnull=True).delete()
 
 
         return instance
